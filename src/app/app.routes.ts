ddc3cf5e0fdef10a302817e0ecdbe98685361bcf
Rolando Vasquez <rolando.sha256@gmail.com>
2025-09-22 00:25:30 -0400

    feat(router+a11y): routes and basic accessibility improvements

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { UiComponent } from './ui/ui';
-
+import { Home } from './home/home';
 
 export const routes: Routes = [
-  
+  { path: '', component: Home },
   { path: 'ui', component: UiComponent }
 ];
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
