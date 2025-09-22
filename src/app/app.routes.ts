4d0cbf2293a3c9d5d05c5ec333c397d7c86c44a5
Rolando Vasquez <rolando.sha256@gmail.com>
2025-09-22 00:11:53 -0400

    debuging

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { UiComponent } from './ui/ui';
 
+
 export const routes: Routes = [
-  { path: 'ui', component: UiComponent },
-  { path: '', pathMatch: 'full', redirectTo: '' }, // ← Sin redirección circular
-  { path: '**', redirectTo: '' }
+  
+  { path: 'ui', component: UiComponent }
 ];
